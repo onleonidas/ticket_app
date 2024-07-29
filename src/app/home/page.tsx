e381cfa3dae67b7015c2511eba635f4e599c6a58
Michel Leonidas <michel.leonidas@redealia.com>
2024-07-28 22:35:52 -0300

    feat: Add HomePage component and TicketKindSelect component

---
+++ src/app/home/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+    return (
+        <div className="bg-gradient-radial from-blue-400 to-blue-500 h-screen flex items-center justify-center">
+            <h1 className="text-white text-4xl font-bold">
+                Hello, TailwindCSS!
+            </h1>
+        </div>
+    );
+}
